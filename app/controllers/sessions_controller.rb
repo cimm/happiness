f76acc4165204247c2412633d9e3cc7931e0e4c6
Simon Schoeters <hamfilter@gmail.com>
2016-05-13 21:46:55 +0200

    Add Google oAuth authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def google_oauth2
+    @google_auth = request.env['omniauth.auth']
+
+    if valid_auth_domain? && employee = find_or_create_employee
+      session[:current_employee] = employee
+      flash[:success] = t('authenticated')
+    else
+      session[:current_employee] = nil
+      flash[:error] = t('not_authenticated')
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def find_or_create_employee
+    Employee.find_by(uid: @google_auth.uid) || Employee.create(email: @google_auth.info.email, uid: @google_auth.uid)
+  end
+
+  def valid_auth_domain?
+    @google_auth.info.email.include?('mymicroinvest.com')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get '/auth/google_oauth2/callback' => 'sessions#google_oauth2'
   root to: 'home#index'
 end
