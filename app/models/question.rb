43b7a77e7c45e265241cc537510a254fe06c2fe2
Simon Schoeters <hamfilter@gmail.com>
2016-05-15 21:59:19 +0200

    Add Question#show

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,22 @@ class Question < ActiveRecord::Base
   validates :body, :regularity, presence: true
   validates :regularity, inclusion: { in: REGULARITIES }
 
+  has_many :answers
+
   scope :fixed_regularity, -> { where regularity: 'fixed' }
   scope :random_regularity, -> { where regularity: 'random' }
+
+  def scores
+    @scores ||= answers.pluck(:score)
+  end
+
+  def happiness_score(since = Time.at(0))
+    [
+      scores.count(0) * -3,
+      scores.count(1) * -2,
+      scores.count(2),
+      scores.count(3) * 2,
+      scores.count(4) * 3
+    ].sum
+  end
 end
