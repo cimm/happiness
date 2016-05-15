4f9dc08685f76782d8355edd6d0bad23ae2bcc77
Simon Schoeters <hamfilter@gmail.com>
2016-05-15 16:53:17 +0200

    Refactor Survey#BatchAnswers#create
    The `sq` variables were still references to the SurveyQuestion object
    which has been renamed to Answer.

--- app/controllers/surveys/batch_answers_controller.rb
+++ app/controllers/surveys/batch_answers_controller.rb
@@ -8,12 +8,12 @@ class Surveys::BatchAnswersController < ApplicationController
 
   def create
     answer_ids = params[:answers].keys
-    result = Answer.update(answer_ids, params[:answers].values).reject{ |sq| sq.errors.empty? }
+    result = Answer.update(answer_ids, params[:answers].values).reject{ |a| a.errors.empty? }
     if result.empty?
       flash[:notice] = t('created')
       redirect_to root_path
     else
-      answer_ids = result.collect{ |sq| sq.id }
+      answer_ids = result.collect{ |a| a.id }
       flash[:error] = answer_ids.join(', ')
       redirect_to new_survey_batch_answer_path(survey: params[:survey_id])
     end
