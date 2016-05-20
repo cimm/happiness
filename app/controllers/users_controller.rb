e71639989f147110ddb4d94e5865baa49e6bc5d2
Simon Schoeters <hamfilter@gmail.com>
2016-05-20 21:47:38 +0200

    Add user deletion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def destroy
+    user = User.find(params[:id])
+    if user != @current_user && user.destroy
+      flash[:notice] = t('deleted')
+    else
+      flash[:error] = t('deleted_error')
+    end
+    redirect_to users_path
+  end
 end
